7b49804bfc808b2291073938361a28456e9d5318
Jonathan Rojo <86434164+JoniRojo@users.noreply.github.com>
2023-05-09 16:08:37 -0300

    Eliminacion carpeta

--- MainSecundario/archivo.h
+++
@@ -1,7 +0,0 @@
-int existe(char estadosRep[20],char i,int cant);
-
-int cantSim(char cadena[20],char i);
-
-int existeSim(char simbolosRep[20],char i,int cant);
-
-int existeFin(char finales[20],char i,int cant);
